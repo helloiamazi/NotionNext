e415ac9332a3e202d2b239dd75f7b0a49bff98ac
helloiamazi <96250923+helloiamazi@users.noreply.github.com>
2022-12-16 12:42:11 +0800

    Update Footer.js

--- themes/hexo/components/Footer.js
+++ themes/hexo/components/Footer.js
@@ -28,7 +28,7 @@ const Footer = ({ title }) => {
         <i className='fas fa-users'/> <span className='px-1 busuanzi_value_site_uv'> </span> </span>
         <br/>
         <h1>{title}</h1>
-        <span className='text-xs '>Powered by <a href='https://github.com/tangly1024/NotionNext' className='underline dark:text-gray-300'>NotionNext {BLOG.VERSION}</a>.</span></span><br/>
+        <span className='text-xs '> 皖ICP备2022008316号 <a href='https://beian.miit.gov.cn/' className='underline dark:text-gray-300'> {BLOG.VERSION}</a>.</span></span><br/>
 
     </footer>
   )
